5ff29c221bf59395ad75a21254eddd891e9b2c27
MunyaradziTayi <128024446+MunyaradziTayi@users.noreply.github.com>
2024-10-11 17:45:53 +1100

    Completed the billing info page
    Signed-off-by: MunyaradziTayi <128024446+MunyaradziTayi@users.noreply.github.com>

---
+++ A2/src/java/com/example/thereadingroom/Orders.java
@@ -0,0 +1,52 @@
+package com.example.thereadingroom;
+
+import java.util.Date;
+
+public class Orders {
+
+    private int order_id;
+    private String customer_name;
+    private Date order_date;
+    private double order_total;
+    private String order_description;
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public Date getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(Date order_date) {
+        this.order_date = order_date;
+    }
+
+    public double getOrder_total() {
+        return order_total;
+    }
+
+    public void setOrder_total(double order_total) {
+        this.order_total = order_total;
+    }
+
+    public String getOrder_description() {
+        return order_description;
+    }
+
+    public void setOrder_description(String order_description) {
+        this.order_description = order_description;
+    }
+}
